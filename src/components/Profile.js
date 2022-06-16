569e6f2ff756d9c1167440c415054c9ec121efda
Kartik Setia <kartset10@gmail.com>
2022-06-16 10:30:40 +0530

    Fixed Profile for smaller screens

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -16,28 +16,28 @@ function Profile() {
         </Box>
         <Box alignSelf="center" px="20" py="16">
         <Text fontSize="xl" fontWeight="bold">
-                Designer and Developer. Specialized in mobile app development
+            Designer and Developer. Specialized in mobile app development
         </Text>
 
-        <Flex direction={isNotSmallerScreen?"row":"column"} mt={8}>
-        <Flex rounded="xl" direction="column" mt={4} mr={2} bg="blue.400" h="30vh" w="30vh" justify="flex-end">
-            <Icon color="white" p="4" as={DiAndroid} w="24" h="24"/>
-            <Text color="white" p="4" fontSize="xl" fontWeight="semibold">
-                Android Apps
-            </Text>
-        </Flex>
-        <Flex rounded="xl" direction="column" mt={4} mr={2} bg="gray.100" h="30vh" w="30vh" justify="flex-end" _hover={{bg: "green.400"}}>
-            <Icon color="black" p="4" as={DiCodeigniter} w="24" h="24"/>
-            <Text color="black" p="4" fontSize="xl" fontWeight="semibold">
-                Flutter Apps
-            </Text>
-        </Flex>
-        <Flex rounded="xl" direction="column" mt={4} bg="gray.100" h="30vh" w="30vh" justify="flex-end" _hover={{bg: "teal.400"}}>
-            <Icon color="black" p="4" as={DiWebplatform} w="24" h="24"/>
-            <Text color="black" p="4" fontSize="xl" fontWeight="semibold">
-                Web Apps
-            </Text>
-        </Flex>
+        <Flex direction={isNotSmallerScreen?"row":"column"} mt={8} minW={'30vh'}>
+            <Flex rounded="xl" direction="column" mt={4} mr={2} bg="blue.400" justify="flex-end">
+                <Icon color="white" p="4" as={DiAndroid} w="24" h="24"/>
+                <Text color="white" p="4" fontSize="xl" fontWeight="semibold">
+                    Android Apps
+                </Text>
+            </Flex>
+            <Flex rounded="xl" direction="column" mt={4} mr={2} bg="gray.100" justify="flex-end" _hover={{bg: "green.400"}}>
+                <Icon color="black" p="4" as={DiCodeigniter} w="24" h="24"/>
+                <Text color="black" p="4" fontSize="xl" fontWeight="semibold">
+                    Flutter Apps
+                </Text>
+            </Flex>
+            <Flex rounded="xl" direction="column" mt={4} bg="gray.100" justify="flex-end" _hover={{bg: "teal.400"}}>
+                <Icon color="black" p="4" as={DiWebplatform} w="24" h="24"/>
+                <Text color="black" p="4" fontSize="xl" fontWeight="semibold">
+                    Web Apps
+                </Text>
+            </Flex>
         </Flex>
         </Box>
     </Flex>
