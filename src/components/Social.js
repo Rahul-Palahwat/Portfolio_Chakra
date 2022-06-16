dd4b2d8e6b8d5f5689130549ef13e5996b42ab6a
Kartik Setia <kartset10@gmail.com>
2022-06-16 10:28:37 +0530

    Shifted to Grid for responsiveness

--- src/components/Social.js
+++ src/components/Social.js
@@ -1,17 +1,18 @@
-import { HStack, Icon } from '@chakra-ui/react'
+import { Icon, Flex, useMediaQuery, Grid } from '@chakra-ui/react'
 import React from 'react'
 
 import {FaFacebook,FaGoogle,FaSpotify,FaShopify} from 'react-icons/fa'
 
 function Social() {
-  return (
-    <HStack spacing={24}>
-        <Icon as={FaFacebook} boxSize={50}></Icon>
-        <Icon as={FaGoogle} boxSize={50}></Icon>
-        <Icon as={FaSpotify} boxSize={50}></Icon>
-        <Icon as={FaShopify} boxSize={50}></Icon>
-    </HStack>
-  )
+  	const [isNotSmallerScreen] = useMediaQuery("(min-width:600px)");
+  	return (
+		<Grid templateColumns={isNotSmallerScreen ? 'repeat(4, 1fr)' : 'repeat(2, 1fr)'} gap={10} templateRows={isNotSmallerScreen ? 'repeat(1, 1fr)' : 'repeat(2, 1fr)' }>
+			<Icon as={FaFacebook} boxSize={50}></Icon>
+			<Icon as={FaGoogle} boxSize={50}></Icon>
+			<Icon as={FaSpotify} boxSize={50}></Icon>
+			<Icon as={FaShopify} boxSize={50}></Icon>
+		</Grid>
+  	)
 }
 
 export default Social
